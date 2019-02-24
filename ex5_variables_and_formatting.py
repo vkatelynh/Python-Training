f00f8f88ccdc0bfcb8497a4106dac6f02969c3bf
vkatelynh <39841104+vkatelynh@users.noreply.github.com>
2019-02-24 08:52:32 -0500

    Add files via upload
    Learn Python the Hard Way by Zed Shaw

---
+++ ex5_variables_and_formatting.py
@@ -0,0 +1,15 @@
+my_name = 'Victoria'
+my_age = 26
+my_height = 62 #inches
+my_weight = 150 #pounds
+my_eyes = 'brown'
+my_hair = 'brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"She is {my_height} inches tall.")
+print(f"She is {my_weight} pounds heavy.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
