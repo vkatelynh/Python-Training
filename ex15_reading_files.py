675b951c59ecae9d8ad8333aa4ec141b1b4825bf
vkatelynh <39841104+vkatelynh@users.noreply.github.com>
2019-02-24 09:51:02 -0500

    Add files via upload

---
+++ ex15_reading_files.py
@@ -0,0 +1,19 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}: ")
+print(txt.read())
+
+print("Type the filename again: ")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
+
+##run this script by calling
+## python 3.6 ex15_reading_files.py ex15sample.txt
+
