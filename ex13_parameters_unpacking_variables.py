7cd35a0daeafe8a1bd87d330a858e4f7dc98c0a4
vkatelynh <39841104+vkatelynh@users.noreply.github.com>
2019-02-24 09:32:37 -0500

    Add files via upload

---
+++ ex13_parameters_unpacking_variables.py
@@ -0,0 +1,11 @@
+from sys import argv
+script, first, second, third = argv
+
+print("This script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+# you run this by calling it as 
+# python3.6 ex13.py first 2nd 3rd
+
