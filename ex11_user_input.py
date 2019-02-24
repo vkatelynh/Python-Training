7c2ce2bef8a8f41efd6a756856b6fae7b763203c
vkatelynh <39841104+vkatelynh@users.noreply.github.com>
2019-02-24 09:22:03 -0500

    Add files via upload

---
+++ ex11_user_input.py
@@ -0,0 +1,12 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So you are {age} years old, {height} tall, and {weight} heavy.")
+
+# end=' ' at the end of each print line
+# puts the input on the same line
+# If omitted, input would go on the line below.
